9ef544ffc63dfc8b6925ad5551aa6a88a5bd387e
Andreas Eidmann <eidmanna@gmail.com>
2023-11-10 21:33:24 +0100

    Update battery.js: schedule based

--- battery.js
+++ battery.js
@@ -1,16 +1,27 @@
 console.log('Start Battery Heat');
-console.log('Battery Heat off');  
-      setState('shelly.0.SHPLG-S#163937#1.Relay0.Switch', false); 
+console.log('Battery Heat off');
+setState('shelly.0.SHPLG-S#163937#1.Relay0.Switch', false);
+
 /*Battery Temperature*/
-on({id: 'bydhvs.0.State.TempBatt', change: 'ne'}, (obj) => {
-    const tempBatt = getState('bydhvs.0.State.TempBatt').val;
-    console.log('Temp Battery: ' +tempBatt);
-    if(tempBatt > 7) {
-      console.log('Battery Heat off');  
-      setState('shelly.0.SHPLG-S#163937#1.Relay0.Switch', false);  
+const handleHeater = () => {
+    const tempBatt = getState('fronius.0.storage.0.Temperature_Cell').val;
+    console.log('Temp Battery: ' + tempBatt);
+    if (tempBatt > 6) {
+        console.log('Battery Heat off');
+        setState('shelly.0.SHPLG-S#163937#1.Relay0.Switch', false);
     }
-    if(tempBatt < 5 ) {
-      console.log('Battery Heat on');    
-      setState('shelly.0.SHPLG-S#163937#1.Relay0.Switch', true);  
+    if (tempBatt < 4) {
+        console.log('Battery Heat on');
+        setState('shelly.0.SHPLG-S#163937#1.Relay0.Switch', true);
     }
-});
+};
+
+schedule(
+    '{"time":{"start":"00:00","end":"23:59","mode":"minutes","interval":10},"period":{"days":1}}',
+    () => {
+        console.log('start heater check');
+        handleHeater();
+    }
+);
+
+handleHeater();
